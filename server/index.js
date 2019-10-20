fc333e356c7c45c16966df63f09c168d2bc8fb6b
asalinasj <andressalinasj.com>
2019-10-19 20:28:35 -0400

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 
 var mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var path = require('path');
 
+var http = require('http');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
@@ -21,6 +23,7 @@ app.use(cors());
 //handle http post requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.urlencoded());
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //setting up mongodb
@@ -34,18 +37,99 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 //db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+const dbName = 'events-react-dev';
+const uri = `mongodb+srv://admin:admin@cluster0-acigc.mongodb.net/test?retryWrites=true&w=majority
+`;
+
+mongoose.connect(uri
+    ).then(() => {
+        console.log('connected to db');
+        app.listen(4000);
+    }).catch(err => {
+        console.log(err);
+    });
+
+connectMongo =() => {
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("HackFSU").collection("Venue");
+
+        // perform actions on the collection object
+        var responseObject = [];
+
+        const newCursor = collection.find();
+        newCursor.forEach(function(res){
+            responseObject.push(res)
+        });
+
+        client.close();
+
+        return responseObject;
+    });
+}
+connectMongo();
+
+//set up schema
+//Define a schema
+var Schema = mongoose.Schema;
+
+var venueSchema = new Schema({
+  name: String,
+  date: String,
+  city: String,
+  state: String
+});
+
+// Compile model from schema
+var VenueModel = mongoose.model('Venue', venueSchema );
+var cursor;
+function getData(){
+    cursor = VenueModel.find();
+    //console.log(cursor);
+    // selecting the 'name' and 'age' fields
+    cursor.select('city state');
+
+    // limit our results to 5 items
+    cursor.limit(5)
+    //console.log(cursor);
+}
+
+function queryData(){
+    db.collection('Venue').find().toArray(function(err, docs) {
+
+        //assert.equal(err, null);
+        //assert.notEqual(docs.length, 0);
+
+        docs.forEach(function(doc) {
+            console.log(doc.name + " is a " + doc.city + " company.");
+        });
+
+        db.close();
+
+    });
+}
+
 app.use("/", (req, res, next) => {
     console.log(__dirname);
     const route = path.join(__dirname, "/views/index.html");
     console.log(route);
     res.sendFile(route);
-})
+});
+
+app.post('/test', (req, res) => {
+    console.log("1: " + res);
+    console.log("2: " + req);
+    console.log("This is: " + res.body.test);
+    
+});
 
 app.get('/test', urlencodedParser, (req, res, next) => {
     console.log(req.body.test);
     console.log(req.params.name);
-})
+});
 
 app.listen(port, () => {
     console.log("Connecting to port");
+    getData();
+    //queryData();
 });
