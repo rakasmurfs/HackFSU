5f0bebfed05496cee5a6f68dc259b409e250008a
Miguel Wolmers <rakasmurfs@gmail.com>
2019-10-19 22:05:00 -0400

    merging with master

---
+++ frontend/src/components/displayAvailable.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './cssSheets/userformstyle.css'
+
+function DisplayAvailable() {
+    var state = ["Car1","Car2","Car3","Car4"];
+    console.log(state);
+    return <div className="form-style-9">
+        <ul>
+            {state.map((temp)=>{
+                return <li>{temp}</li>
+            })}
+        </ul>
+    </div>
+}
+
+export default DisplayAvailable;
